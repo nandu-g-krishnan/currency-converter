5906d5c44e79b0b5c22a1387a01f3ed478a6bfc3
NGK <59110984+nandu-g-krishnan@users.noreply.github.com>
2021-10-24 16:31:48 +0530

    added firebase database and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/shared/auth.gaurd';
+import { ConverterComponent } from './converter/converter.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/sign-in', pathMatch: 'full'},
+{ path: 'sign-in', component: SignInComponent},
+ { path: 'register-user', component: SignUpComponent},
+ { path: 'converter', component: ConverterComponent, canActivate: [AuthGuard]  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDG9DsWvJ2K-LmARZspxErNRus6feDQxM0",
+    authDomain: "currency-converter-b788c.firebaseapp.com",
+    projectId: "currency-converter-b788c",
+    storageBucket: "currency-converter-b788c.appspot.com",
+    messagingSenderId: "590516870164",
+    appId: "1:590516870164:web:6bac026dfa91a9ab321b01",
+    measurementId: "G-MELX7ZBW58"
+  }
 };
